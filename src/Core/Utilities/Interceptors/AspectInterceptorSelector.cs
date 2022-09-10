792546e873cfed89aececec5bb872ec55324d4ff
Muhammet Dinç <gresta.bilisim@gmail.com>
2022-09-11 01:24:42 +0300

    Commit 6 | Core -> Utilities --> Interceptors Files Created

---
+++ src/Core/Utilities/Interceptors/AspectInterceptorSelector.cs
@@ -0,0 +1,22 @@
+﻿using Castle.Core.Interceptor;
+using Core.Aspects.Autofac.Exception;
+using Core.CrossCuttingConcerns.Logging.Log4Net.Loggers;
+using System.Reflection;
+
+
+namespace Core.Utilities.Interceptors
+{
+    public class AspectInterceptorSelector : IInterceptorSelector
+    {
+        public IInterceptor[] SelectInterceptors(Type type, MethodInfo method, IInterceptor[] interceptors)
+        {
+            var classAttributes = type.GetCustomAttributes<MethodInterceptionBaseAttribute>
+                (true).ToList();
+            var methodAttributes = type.GetMethod(method.Name)
+                .GetCustomAttributes<MethodInterceptionBaseAttribute>(true);
+            classAttributes.AddRange(methodAttributes);
+
+            return classAttributes.OrderBy(x => x.Priority).ToArray();
+        }
+    }
+}

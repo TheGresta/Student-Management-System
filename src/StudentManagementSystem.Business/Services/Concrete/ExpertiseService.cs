378724d2e13f80c1fef3df7c087f75daed4bfdf6
Muhammet Dinç <gresta.bilisim@gmail.com>
2022-09-11 19:06:52 +0300

    Commit 18 | Business -> ExpertiseService Created

---
+++ src/StudentManagementSystem.Business/Services/Concrete/ExpertiseService.cs
@@ -0,0 +1,51 @@
+﻿using Core.Aspects.Validation;
+using Core.Paging;
+using Core.Utilities.Message;
+using Core.Utilities.Result;
+using Microsoft.EntityFrameworkCore.Query;
+using StudentManagementSystem.Business.Services.Abstract;
+using StudentManagementSystem.DataAccess.Abstract;
+using StudentManagementSystem.Dto.Concrete.Expertise;
+using StudentManagementSystem.Entity.Concrete;
+using System.Linq.Expressions;
+
+namespace StudentManagementSystem.Business.Services.Concrete
+{
+    public class ExpertiseService : BaseManager<Expertise, ExpertiseWriteDto, ExpertiseReadDto>, IExpertiseService
+    {
+        public ExpertiseService(IExpertiseDal repository, ILanguageMessage languageMessage) : base(repository, languageMessage)
+        {
+        }
+
+        public override async Task<DataResult<ExpertiseReadDto>> AddAsync(ExpertiseWriteDto writeDto)
+        {
+            return await base.AddAsync(writeDto);
+        }
+
+        public async override Task<DataResult<ExpertiseReadDto>> UpdateAsync(int id, ExpertiseWriteDto writeDto)
+        {
+            return await base.UpdateAsync(id, writeDto);
+        }
+
+        public override async Task<DataResult<ExpertiseReadDto>> DeleteAsync(int id)
+        {
+            return await base.DeleteAsync(id);
+        }
+
+        public override async Task<DataResult<ExpertiseReadDto>> GetByIdAsync(int id)
+        {
+            return await base.GetByIdAsync(id);
+        }
+
+
+        public override async Task<DataResult<IPaginate<ExpertiseReadDto>>> GetListAsync(
+            Expression<Func<Expertise, bool>>? predicate = null,
+            Func<IQueryable<Expertise>, IOrderedQueryable<Expertise>>? orderBy = null,
+            Func<IQueryable<Expertise>, IIncludableQueryable<Expertise, object>>?
+            include = null, int index = 0, int size = 10, bool enamleTracking = true,
+            CancellationToken cancellationToken = default)
+        {
+            return await base.GetListAsync(predicate, orderBy, include, index, size, enamleTracking, cancellationToken);
+        }
+    }
+}
